e1cd309407d05a661d3e16ac4d208ccf19f7a11c
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-10-25 21:28:42 -0300

    add further card stuff

--- dia25-10-21/JS/appendCards.js
+++ dia25-10-21/JS/appendCards.js
@@ -4,15 +4,16 @@ const appendCards = (pokemons, cardsArea) => {
 		const pokemonImg = pokemon.sprites.front_default;
 
 		const attributes = pokemon.stats;
-		console.log(pokemon.types[0].type.name);
+
+		console.log(attributes);
 
 		accumulator += `<div class="card ${pokemon.types[0].type.name}">
 		<img src='${pokemonImg}' />
 		<table>
-			<tr><td>Life</td><td>90</td></tr>
-			<tr><td>Attack</td><td>90</td></tr>
-			<tr><td>Defense</td><td>90</td></tr>
-			<tr><td>Speed</td><td>90</td></tr>
+			<tr><td>Life</td><td>${attributes[0].base_stat}</td></tr>
+			<tr><td>Attack</td><td>${attributes[1].base_stat}</td></tr>
+			<tr><td>Defense</td><td>${attributes[2].base_stat}</td></tr>
+			<tr><td>Speed</td><td>${attributes[5].base_stat}</td></tr>
 		</table>
 	</div>`;
 
