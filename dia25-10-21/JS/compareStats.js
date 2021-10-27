6188ed3e3b8ec9cd49bf57b5a08890fe293319ba
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-10-27 18:07:55 -0300

    the comparison is now logging the champion

--- dia25-10-21/JS/compareStats.js
+++ dia25-10-21/JS/compareStats.js
@@ -2,20 +2,25 @@ const compareStats = (statProfiles) => {
 	const listItem = document.querySelector('.attribute-selector');
 	const statToCompare = listItem.value;
 	const resultSpan = document.querySelector('.result');
+	let champion = null;
+	let initialValue = 0;
 
 	if (listItem.value) {
-		let champion = null;
-		let initialValue = 0;
-
 		if (statToCompare === 'hp') {
 			resultSpan.textContent = 'Draw!';
 		} else {
 			Object.entries(statProfiles).forEach((entry) => {
 				const [key, value] = entry;
-				console.log(key, value);
+
+				if (value[statToCompare] > initialValue) {
+					champion = key;
+					initialValue = value[statToCompare];
+				}
 			});
 		}
 	}
+
+	console.log(champion);
 };
 
 export default compareStats;
